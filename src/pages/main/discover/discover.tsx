41d61ab26a1f61b227cc0c5e8d98a4c38fc92830
Dwiki Baskoro <dwikibaskoro888@gmail.com>
2020-09-14 16:37:51 +0700

    fix: change icon

--- src/pages/main/discover/discover.tsx
+++ src/pages/main/discover/discover.tsx
@@ -8,7 +8,7 @@ import {
 } from 'react-native'
 import HorizontalListLookBook from '@components/organisms/horzontal-list-lookbook'
 import Feed from '@components/organisms/discover-feed'
-import Icon from 'react-native-vector-icons/FontAwesome'
+import Icon from '@assets/fonts/custom-icons'
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import { colors } from '@utils/constants'
 import DiscoverTab from '@src/components/molecules/tab-menu-home'
